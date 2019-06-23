000d5815c84d0b1c90ee00f11e0b17a77eba5ab8
jbulls2 <52134793+jbulls2@users.noreply.github.com>
2019-06-24 01:58:13 +0900

    Add files via upload

---
+++ MapleMacro/MapleMacro/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace MapleMacro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Macro1Button = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("휴먼둥근헤드라인", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(530, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MapleStoryMacro";
+            // 
+            // Macro1Button
+            // 
+            this.Macro1Button.Location = new System.Drawing.Point(278, 81);
+            this.Macro1Button.Name = "Macro1Button";
+            this.Macro1Button.Size = new System.Drawing.Size(115, 23);
+            this.Macro1Button.TabIndex = 1;
+            this.Macro1Button.Text = "동굴윗쪽길 (썬콜)";
+            this.Macro1Button.UseVisualStyleBackColor = true;
+            this.Macro1Button.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(475, 233);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 84);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(231, 20);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            this.comboBox1.Click += new System.EventHandler(this.ComboBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 268);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.Macro1Button);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "천사네 매크로♥";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Macro1Button;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ MapleMacro/MapleMacro/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProcessMemoryReaderLib;
+
+namespace MapleMacro
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        Process[] MyProcess;
+        ProcessMemoryReader mem = new ProcessMemoryReader();
+        bool attach = false;
+
+        private static DateTime Delay(int MS)
+
+        {
+
+            DateTime ThisMoment = DateTime.Now;
+
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
+
+            DateTime AfterWards = ThisMoment.Add(duration);
+
+            while (AfterWards >= ThisMoment)
+
+            {
+
+                System.Windows.Forms.Application.DoEvents();
+
+                ThisMoment = DateTime.Now;
+
+            }
+
+            return DateTime.Now;
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            bool trueOrfalse = true;
+            int delay1 = r.Next(500, 530);
+
+            Delay(5000);
+            while (trueOrfalse)
+            {
+                SendKeys.Send("{a}");
+                Delay(delay1);
+                SendKeys.Send("{a}");
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            result = MessageBox.Show("종료하시겠습니까?", "종료메시지", MessageBoxButtons.OKCancel);
+
+            if(result == DialogResult.OK)
+            {
+                this.DialogResult = DialogResult.Abort;
+                Application.Exit();
+            }
+        }
+
+        private void ComboBox1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            MyProcess = Process.GetProcesses();
+
+            for(int i = 0; i < MyProcess.Length; i++)
+            {
+                String text = MyProcess[i].ProcessName + "-" + MyProcess[i].Id;
+                comboBox1.Items.Add(text);
+            }
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedIndex != -1)
+                {
+                    String selectedItem = comboBox1.SelectedItem.ToString();
+                    int pid = int.Parse(selectedItem.Split('-')[selectedItem.Split('-').Length - 1]);
+                    Process attachProc = Process.GetProcessById(pid);
+
+                    mem.ReadProcess = attachProc;
+                    mem.OpenProcess();
+                    attach = true;
+
+                    MessageBox.Show("프로세스 열기 성공! " + attachProc.ProcessName);
+
+                }
+            }
+            catch(Exception ex)
+            {
+                attach = false;
+
+                MessageBox.Show("프로세스 열기 실패! " + ex.Message);
+            }
+        }
+    }
+}
